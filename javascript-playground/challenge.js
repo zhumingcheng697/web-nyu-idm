928e98001d8853f966a3b0c0896677e219348c6a
McCoy Zhu <mz1945@nyu.edu>
2020-03-12 14:42:54 -0400

    add js playground

---
+++ javascript-playground/challenge.js
@@ -0,0 +1,24 @@
+//Welcome to the in-class challenge!
+
+let myAlphabet = ['A', 'B', 'C', 'D','E','F','G'];
+
+// 1. What is the length of the array?
+// push your edits to github!!
+
+// 2. Write a function called myAlphabetLength which console.logs the length of the array
+// push your edits to github!!
+
+// 3. Within the function also use an if-conditional statement that checks if the number of items within the array are less than 4
+// push your edits to github!!
+
+// 4. Declare and initialize an array called 'colors' with 5 different string values representing the names of 5 colors of your choosing
+// push your edits to github!!
+
+// 5.  Console.log each item in the array
+// push your edits to github!!
+
+// 6. Declare an empty string variable called 'text'. Each time you console.log a color from the above challenge number 2, add your color to this variable.
+// push your edits to github!!
+
+//7. Create a div in the index.html with a unique id called whatever you want. Display the contents of your text variable to this div. Now you should have a list of 5 colors visible on your website.
+// push your edits to github!!
---
+++ javascript-playground/demo.js
@@ -0,0 +1,5 @@
+// An array is a special variable, which can hold more than one value at a time.
+// Understanding the difference between let & var: It can be said that a variable declared with var is defined throughout the program (function scoped) as compared to let (which is block scoped). A function can contain many blocks.
+// watch later: https://www.youtube.com/watch?v=XgSjoHgy3Rk
+
+let namesArray = ['Kevin','McCoy','Emily','Frederic','Alisha','Ashli','Simon','Ty','Fiona','Prismo','Sammy','Caroline','Francesca','Karl','Esther','Irene','Martin','Yanxin','Katrina','Kiana'];
