4fa8b6064de2aaa9fc5843caee9236322f8fa615
McCoy Zhu <mz1945@nyu.edu>
2020-03-26 12:23:33 -0700

    challenge 4

--- javascript-playground/challenge.js
+++ javascript-playground/challenge.js
@@ -27,13 +27,15 @@ function myAlphabetLengthWithTest() {
 myAlphabetLengthWithTest();
 
 // 4. Declare and initialize an array called 'colors' with 5 different string values representing the names of 5 colors of your choosing
-// push your edits to github!!
+let colors = ["red", "orange", "yellow", "green", "cyan"];
 
-// 5.  Console.log each item in the array
+console.log(colors);
+
+// 5. Console.log each item in the array
 // push your edits to github!!
 
 // 6. Declare an empty string variable called 'text'. Each time you console.log a color from the above challenge number 2, add your color to this variable.
 // push your edits to github!!
 
-//7. Create a div in the index.html with a unique id called whatever you want. Display the contents of your text variable to this div. Now you should have a list of 5 colors visible on your website.
+// 7. Create a div in the index.html with a unique id called whatever you want. Display the contents of your text variable to this div. Now you should have a list of 5 colors visible on your website.
 // push your edits to github!!
